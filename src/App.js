5e7d434235f90b54a5ae08ff94ebc8b0c0c86796
bhuvaneshwaran-kumar <bhuvaneshwarankumar2000@gmail.com>
2021-07-28 10:58:59 +0530

    setted up template for redux - 🌍🌍🌍

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
+import { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { setUser } from './actions/index'
 function App() {
+  const user = useSelector((state) => state.user)
+  const dispatch = useDispatch()
+  // console.log(user)
+
+
+  useEffect(() => {
+
+    dispatch(setUser("bhuvan"))
+    console.log(user)
+  }, [])
+
+  const handleClick = () => {
+    // console.log('mani')
+    dispatch(setUser("mani"))
+  }
   return (
-    <h1>HellowWorld!....</h1>
+    <>
+      <h1>Hello World!....{user}</h1>
+      <button onClick={handleClick}>Change user</button>
+    </>
   );
 }
 
