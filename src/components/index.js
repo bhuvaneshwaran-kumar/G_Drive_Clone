1aee8f0c89ce2335286142ddc86fa3ecf934d636
bhuvaneshwaran-kumar <bhuvaneshwarankumar2000@gmail.com>
2021-08-01 19:24:21 +0530

    reFactoring to named Export

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import Nav from './Nav'
+import CreateAlbumModal from './CreateAlbumModal'
+import Login from './Login'
+import Photo from './Photo'
+import Album from './Album'
+
+export {
+    Nav,
+    CreateAlbumModal,
+    Login,
+    Photo,
+    Album,
+}
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import AlbumPage from './AlbumPage'
+import HomePage from './HomePage'
+export {
+    AlbumPage,
+    HomePage
+}
